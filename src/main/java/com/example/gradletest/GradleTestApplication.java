66c2cc5ebf6640349198534e1272141f6c9fed9d
ziogenorwekh <ziogenorwekh@gmail.com>
2023-04-21 23:06:36 +0900

    test ok

---
+++ src/main/java/com/example/gradletest/GradleTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleTestApplication.class, args);
+    }
+
+}
